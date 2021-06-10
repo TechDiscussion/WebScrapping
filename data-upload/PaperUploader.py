9658d9ef89e5e1c4c1d743abef9efa98eb99962e
siddhant <siddhantkhare2694@gmail.com>
2021-06-10 11:55:43 +0530

    Fetch All CS Topics and Save to a text file

--- data-upload/PaperUploader.py
+++ data-upload/PaperUploader.py
@@ -3,9 +3,9 @@
 import arxiv
 
 search = arxiv.Search(
-    query="Machine Learning",
-    max_results=10,
-    sort_by=arxiv.SortCriterion.SubmittedDate
+    query="all: Computer Science (cs)",
+    max_results=700,
+    sort_by=arxiv.SortCriterion.SubmittedDate,
 )
 
 for result in search.get():
