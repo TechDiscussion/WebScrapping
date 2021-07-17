b3ce18321349c4b677274582d27f668658cc4dd7
Chedvihas <62206587+Chedvihas@users.noreply.github.com>
2021-07-17 17:56:20 +0530

    Update blog data (#16)
    * Added data of 3 blogs
    
    * Added 14 blogs
    
    * Date format fixed and added new websites
    
    * Added 50 websites
    
    * Fixed abstract in FindMyPast.json
    
    * Fixed json format
    
    * Add files via upload
    
    * Code to Scrape data
    
    * Delete Data-Upload.py
    
    * Code to Auto update Data
    
    * Added script to update blog data automatically
    
    * Update requirements.txt
    
    * Deleted unnecessary binary files
    
    * Delete log.txt
    
    * Changing database to test
    
    * Removed unnecessary files

---
+++ Auto_Update_Data/Auto_Update_Data/pipelines.py
@@ -0,0 +1,42 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+from pymongo import MongoClient
+import uuid
+from .appendkeywords import keywordsFromBlog, keywords
+
+class AutoUpdateDataPipeline:
+    def clean_author(self, author):
+        if (author != "Unknown"):
+            author = [i.strip() for i in author if i]
+        return author
+
+    def __init__(self):
+        self.conn = MongoClient(
+            "mongodb+srv://chedvi:c@cluster0.kf3n4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
+
+        # db = self.conn['TechVault']
+        # self.collection = db['contents']
+        db = self.conn['testing_upload']
+        self.collection = db['Blog_Testing']
+    def process_item(self, item, spider):
+        item['totalComments'] = 0
+        item['totalViews'] = 0
+        item['totalLikes'] = 0
+        item['company'] = item['website']
+        del item['website']
+        item['full_content'] = item['abstract']
+        item['type'] = 'blog'
+        item['author'] = self.clean_author(item['author'])
+        item['abstract'] = item['abstract'][0:900]
+        item['keywords'] = keywordsFromBlog(item['full_content'], keywords)
+
+        item['uuid'] = uuid.uuid4().hex
+        self.collection.insert_one(dict(item))
+        return item
